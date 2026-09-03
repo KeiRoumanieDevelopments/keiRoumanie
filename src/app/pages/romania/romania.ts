4b936afec1cd8ee329a5c9ea4f491bed73b89e28
AggelosQubit <aggelosqubit@gmail.com>
2026-09-03 17:49:17 +0200

    Feature / Page - La Roumanie

--- src/app/pages/romania/romania.ts
+++ src/app/pages/romania/romania.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { PagePlaceholder } from '../../shared/page-placeholder/page-placeholder';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-romania',
-  imports: [PagePlaceholder],
+  imports: [RouterLink],
   templateUrl: './romania.html',
   styleUrl: './romania.css',
   host: { class: 'page-route' },
