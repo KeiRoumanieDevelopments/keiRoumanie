4f781deb4b0712601bd6ccf24c0aae6aa6a74937
AggelosQubit <aggelosqubit@gmail.com>
2026-09-03 15:04:33 +0200

    Feature / Page - A Propos

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { PagePlaceholder } from '../../shared/page-placeholder/page-placeholder';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [PagePlaceholder],
+  imports: [RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
   host: { class: 'page-route' },
